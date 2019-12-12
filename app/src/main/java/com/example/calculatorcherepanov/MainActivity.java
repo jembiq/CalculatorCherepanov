0a44e8acf16b8f75142cdee2aa2fe78aaf4f180e
jembiq <vadim.vadim4@yandex.ru>
2019-12-12 22:26:37 +0300

    new proj

---
+++ app/src/main/java/com/example/calculatorcherepanov/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculatorcherepanov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
